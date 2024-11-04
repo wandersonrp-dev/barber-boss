c7e7cef954c549fdee8d2a9152ad50ed0269c34d
wandersonrp-dev <wandersonrp.dev@outlook.com>
2024-11-04 15:07:24 -0300

    ✨ feat: add custom authentication state provider

---
+++ src/Web/BarberBoss.Web/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,96 @@
+﻿using BarberBoss.Web.Apis.Auth;
+using BarberBoss.Web.Apis.BarberShops;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Refit;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace BarberBoss.Web;
+public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILocalStorageService _localStorage;
+
+    public CustomAuthenticationStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+    {
+        _httpClient = httpClient;
+        _localStorage = localStorage;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var token = await _localStorage.GetItemAsync<string>("auth_token");
+
+        if (string.IsNullOrWhiteSpace(token))
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+
+        var refitSettings = new RefitSettings
+        {
+            AuthorizationHeaderValueGetter = (httpMessage, cancelationToken) => Task.FromResult(token)
+        };
+
+        RestService.For<IBarberShopApi>(_httpClient, refitSettings);
+
+        return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt")));        
+    }
+
+    public void MarkUserAsAuthenticated(string email)
+    {
+        var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[]
+        {
+               new Claim(ClaimTypes.Email, email)
+            }, "apiauth"));
+
+        var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+        NotifyAuthenticationStateChanged(authState);
+    }
+
+    public void MarkUserAsLoggedOut()
+    {
+        var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
+        var authState = Task.FromResult(new AuthenticationState(anonymousUser));
+
+        NotifyAuthenticationStateChanged(authState);
+    }
+
+    private IEnumerable<Claim>? ParseClaimsFromJwt(string token)
+    {
+        var claims = new List<Claim>();
+        var payload = token.Split('.')[1];
+        var jsonBytes = ParseBase64WithoutPadding(payload);
+        var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);        
+
+        if (keyValuePairs is null)
+            return claims;        
+
+        foreach (var kvp in keyValuePairs)
+        {
+            if (kvp.Value is JsonElement jsonElement && jsonElement.ValueKind == JsonValueKind.Array)
+            {
+                foreach (var item in jsonElement.EnumerateArray())
+                {                    
+
+                    claims.Add(new Claim(kvp.Key, item.ToString()));
+                }
+            }
+            else
+            {
+                claims.Add(new Claim(kvp.Key, kvp.Value.ToString()!));
+            }
+        }       
+
+        return claims;
+    }
+
+    private byte[] ParseBase64WithoutPadding(string base64)
+    {
+        switch (base64.Length % 4)
+        {
+            case 2: base64 += "=="; break;
+            case 3: base64 += "="; break;
+        }
+
+        return Convert.FromBase64String(base64);
+    }
+}
--- src/Web/BarberBoss.Web/Program.cs
+++ src/Web/BarberBoss.Web/Program.cs
@@ -1,5 +1,7 @@
 using BarberBoss.Web;
 using BarberBoss.Web.Apis.BarberShops;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Refit;
@@ -8,12 +10,18 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
 builder.Services.AddOidcAuthentication(options =>
 {    
     builder.Configuration.Bind("Local", options.ProviderOptions);
 });
 
+builder.Services.AddAuthorizationCore();
+
 ConfigureRefit(builder.Services, builder.Configuration);
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
 
