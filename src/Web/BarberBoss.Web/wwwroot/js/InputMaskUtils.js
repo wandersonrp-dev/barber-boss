32334840d67fe3b43c5a5d326f2bbaa969afcf1f
wandersonrp-dev <wandersonrp.dev@outlook.com>
2024-11-06 23:13:56 -0300

    🐛 fix: get unmasked phone to update profile

---
+++ src/Web/BarberBoss.Web/wwwroot/js/InputMaskUtils.js
@@ -0,0 +1,7 @@
+﻿window.getUnmaskedPhoneValue = (elementId) => {    
+    const unmaskedPhone = IMask(document.getElementById(elementId), { mask: "(00) 0 0000-0000" }).unmaskedValue
+
+    if (!unmaskedPhone) return;
+    
+    return unmaskedPhone;
+};
