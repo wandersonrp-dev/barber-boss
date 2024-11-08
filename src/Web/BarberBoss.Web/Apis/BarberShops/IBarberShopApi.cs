23db0447164abbfa4441ec813353ebfeef39758d
wandersonrp-dev <wandersonrp.dev@outlook.com>
2024-11-08 11:33:20 -0300

    🐛 fix: error and success toasts presentation

--- src/Web/BarberBoss.Web/Apis/BarberShops/IBarberShopApi.cs
+++ src/Web/BarberBoss.Web/Apis/BarberShops/IBarberShopApi.cs
@@ -17,6 +17,6 @@ public interface IBarberShopApi
     [Patch("/api/barber-shops/profile")]
     Task<IApiResponse> UpdateProfile(RequestBarberShopJson request);
 
-    [Patch("/api/barber-shops/barbers")]
+    [Post("/api/barber-shops/barbers")]
     Task<IApiResponse> CreateBarber(RequestCreateBarberJson request);
 }
