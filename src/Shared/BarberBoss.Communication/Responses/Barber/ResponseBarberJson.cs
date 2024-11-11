d91668b1041d5ba2979e51ba51467e8e811685fc
wandersonrp-dev <wandersonrp.dev@outlook.com>
2024-11-11 12:31:33 -0300

    ✨ feat: add get barber profile and barber changed password modal

--- src/Shared/BarberBoss.Communication/Responses/Barber/ResponseBarberJson.cs
+++ src/Shared/BarberBoss.Communication/Responses/Barber/ResponseBarberJson.cs
@@ -7,6 +7,7 @@ public record ResponseBarberJson
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
+    public string? Password { get; set; }
     public bool ChangedInitialPassword { get; set; }
     public UserStatus UserStatus { get; set; }
     public DateTime CreatedAt { get; set; }
